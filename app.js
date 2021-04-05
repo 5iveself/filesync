8aa8537db8a81dc8fc357afed69d93d7eb2d983c
5iveself <78660200+5iveself@users.noreply.github.com>
2021-04-05 18:41:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+VANTA.NET({
+  el: "#preview",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff,
+  backgroundColor: 0xffffff
+})
+
+VANTA.NET({
+  el: "#about",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff00ff,
+  backgroundColor: 0xffffff
+})
